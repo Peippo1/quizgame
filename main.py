8c66b79932e9e3a350c59f620107598d312fca20
Peippo <tim.o.finch@gmail.com>
2021-09-17 14:14:46 +0100

    final score

--- main.py
+++ main.py
@@ -14,3 +14,6 @@ quiz = QuizBrain(question_bank)
 # Continue asking questions?
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the quiz \n")
+print(f"Your final score is {quiz.score}/{len(question_bank)}")
