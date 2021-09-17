0ff9ba8d3032b8e7a4cd96790ca4859384e5eb32
Peippo <tim.o.finch@gmail.com>
2021-09-17 12:29:32 +0100

    check answer function added

--- quiz_brain.py
+++ quiz_brain.py
@@ -11,4 +11,14 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        # correct answer outside of code block to print regardless fo the user input
+
