864d95431df2c488f6b305addc4608199502ac53
Peippo <tim.o.finch@gmail.com>
2021-09-17 12:05:45 +0100

    while for number of questions remaining

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,6 +4,11 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        input(f"Q.{self.question_number} {current_question.text} (True/False")
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
