2564c82fa919bd1787be19d75ae518943ed576f7
Peippo <tim.o.finch@gmail.com>
2021-09-17 15:30:26 +0100

    Update of question data

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
@@ -17,3 +17,4 @@ while quiz.still_has_questions():
 
 print("You've completed the quiz \n")
 print(f"Your final score is {quiz.score}/{len(question_bank)}")
+ 
