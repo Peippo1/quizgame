2a51c2902c66706d57242d04fe027636c1dc002b
Peippo <tim.o.finch@gmail.com>
2021-09-17 14:01:52 +0100

    Score added

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
     
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def still_has_questions(self):
@@ -16,9 +17,12 @@ class QuizBrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
+            self.score += 1
             print("You got it right!")
+
         else:
             print("That's wrong.")
         print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
         # correct answer outside of code block to print regardless fo the user input
 
